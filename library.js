2dbe543172e516d3554d5e6ce1b8a32e7e2ad4a3
fubiknees <85858509+fubiknees@users.noreply.github.com>
2024-06-03 22:23:29 -0700

    Check btn functionality then work on array.

--- library.js
+++ library.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+let newBookbtn = document.querySelector('#new-book-btn');
+newBookbtn.addEventListener('click', function(){
+    alert('hello world');
+})
+
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
